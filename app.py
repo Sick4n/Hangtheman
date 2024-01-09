e16128c76df12624a0c1d58c03ec44a20058986b
Hector Bjernersjö <sick4n@protonmail.com>
2024-01-09 20:10:20 +0100

    updated app, added 404, added Gunicorn to require

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
+from flask import Flask, render_template, request, session
 import random
-from flask import Flask, render_template, request
-
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-word_list = ["apple", "banana", "orange", "grape", "cherry"]
-
-def get_word():
-    word = random.choice(word_list)
-    return word.upper()
+words = ["APPLE", "BANANA", "CHERRY", "ORANGE", "GRAPE", "PEAR"] 
 
 def display_hangman(tries):
     stages = [
@@ -83,56 +79,94 @@ def display_hangman(tries):
     ]
     return stages[tries]
 
-@app.route('/')
+
+def get_display_word(word, guessed_letters):
+    display_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    return display_word
+
+def has_guessed_all_letters(word, guessed_letters):
+    for letter in word:
+        if letter not in guessed_letters:
+            return False
+    return True
+
+def render_play(message, additional_context):
+    context = {
+        "message": message,
+        "guessed_letters": session["guessed_letters"],
+        "guessed_words": session["guessed_words"],
+        "display_word": session["display_word"],
+        "hangman": display_hangman(session["lives"])
+        }
+    context.update(additional_context)
+    return render_template("play.html", **context)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(Exception)
+def internal_error(error):
+    # Log the error
+    app.logger.error('Server Error: %s', (error))
+    # Return a generic error message
+    return "An internal error occurred. Please try again later.", 500
+
+@app.route("/")
 def index():
-    word = get_word()
-    return render_template('index.html', word=word, hangman=display_hangman(6), word_completion="_" * len(word))
+    return render_template("index.html")
 
-@app.route('/play', methods=['POST'])
+@app.route("/play", methods=["GET", "POST"])
 def play():
-    word = request.form['word'].upper()
-    word_completion = request.form['word_completion']
-    guessed_letters = request.form.getlist('guessed_letters[]')
-    guessed_words = request.form.getlist('guessed_words[]')
-    tries = int(request.form['tries'])
-    guess = request.form['guess'].upper()
+    if request.method == "POST":
+        guess = request.form["guess"]
+        guess = guess.upper()
+        message = ""
+        if guess in session["guessed_letters"] or guess in session["guessed_words"]:
+            message = f"You already guessed {guess}!"
+            return render_play(message, {})
 
-    if len(guess) == 1 and guess.isalpha():
-        if guess in guessed_letters:
-            message = f"You already guessed the letter {guess}"
-        elif guess not in word:
-            message = f"{guess} is not in the word."
-            tries -= 1
-            guessed_letters.append(guess)
-        else:
-            message = f"Good job, {guess} is in the word!"
-            guessed_letters.append(guess)
-            word_as_list = list(word_completion)
-            indices = [i for i, letter in enumerate(word) if letter == guess]
-            for index in indices:
-                word_as_list[index] = guess
-            word_completion = "".join(word_as_list)
-            if "_" not in word_completion:
-                return render_template('win.html', word=word)
-    elif len(guess) == len(word) and guess.isalpha():
-        if guess in guessed_words:
-            message = f"You already guessed the word {guess}"
-        elif guess != word:
-            message = f"{guess} is not the word."
-            tries -= 1
-            guessed_words.append(guess)
-        else:
-            return render_template('win.html', word=word)
-    else:
-        message = "Not a valid guess."
+        if len(guess) == 1 and guess.isalpha():
+            session["guessed_letters"].append(guess)
+            session["display_word"] = get_display_word(session["word"], session["guessed_letters"])
+            if guess in session["word"]:
+                if has_guessed_all_letters(session["word"], session["guessed_letters"]):
+                    return render_template("win.html", word=session["word"])
+                message = f"{guess} is in the word!"
+            else:
+                message= f"{guess} is not in the word!"
+                session["lives"] -= 1
 
-    hangman = display_hangman(tries)
+        elif len(guess) == len(session["word"]) and guess.isalpha():
+            session["guessed_words"].append(guess)
+            if guess == session["word"]:
+                return render_template("win.html", word=session["word"])
+            else:
+                message = f"{guess} is not the word!"
+                session["lives"] -= 1
+                
+        elif guess.isalpha() == False:
+            message = "That is not a valid letter or word."
+            
+        elif len(guess) != 1 and len(guess) != len(session["word"]):
+            message = "The guess must be one letter or the same length as the word."
 
-    if tries == 0:
-        return render_template('lose.html', word=word)
+        if session["lives"] == 0:
+            return render_template("lose.html", word=session["word"])
 
-    return render_template('play.html', word=word, hangman=hangman, word_completion=word_completion, message=message, tries=tries, guessed_letters=guessed_letters, guessed_words=guessed_words)
+        return render_play(message, {})
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    
+
+    elif request.method == "GET":
+        session["word"] = random.choice(words)
+        session["guessed_letters"] = []
+        session["guessed_words"] = []
+        session["lives"] = 6
+        session["display_word"] = get_display_word(session["word"], session["guessed_letters"])
+        return render_play("Welcome to Hangman! Guess a letter or the whole word.", {})
+
